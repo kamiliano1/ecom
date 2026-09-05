966fbaa85ee2832895ea00005e8da172a3b1420a
kamiliano1 <kamiliano1991@gmail.com>
2026-09-05 11:27:15 +0200

    feat: complete monolith

--- src/main/java/com/app/ecom/EcomApplication.java
+++ src/main/java/com/app/ecom/EcomApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcomApplication.class, args);
-	}
+	
+    public static void main(String[] args) {
+        SpringApplication.run(EcomApplication.class, args);
+    }
 
 }
