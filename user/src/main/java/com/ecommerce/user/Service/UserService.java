65b3c1003615e0bb1a93d6519546668a70479e64
kamiliano1 <kamiliano1991@gmail.com>
2026-09-08 14:58:36 +0200

    feat: Connected User to MongoDB, connected Product and Order to PostgresSQL

--- user/src/main/java/com/ecommerce/user/Service/UserService.java
+++ user/src/main/java/com/ecommerce/user/Service/UserService.java
@@ -7,8 +7,6 @@ import com.ecommerce.user.dto.AddressDto;
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
