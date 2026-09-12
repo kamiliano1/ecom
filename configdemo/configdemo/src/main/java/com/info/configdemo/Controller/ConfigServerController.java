a1267b3cf836675bfb0541cb8a87666f52349350
kamiliano1 <kamiliano1991@gmail.com>
2026-09-12 19:17:59 +0200

    feat: added config from properites from private repo and refresh using actuator

--- configdemo/configdemo/src/main/java/com/info/configdemo/Controller/ConfigServerController.java
+++ configdemo/configdemo/src/main/java/com/info/configdemo/Controller/ConfigServerController.java
@@ -3,11 +3,13 @@ package com.info.configdemo.Controller;
 import com.info.configdemo.Configuration.ConfigServer;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RefreshScope
 public class ConfigServerController {
     private final ConfigServer configServer;
 
