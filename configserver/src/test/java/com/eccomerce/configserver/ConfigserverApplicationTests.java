f2df72803d0ac5dddc5cf41a162c62bc188562b5
kamiliano1 <kamiliano1991@gmail.com>
2026-09-16 20:25:20 +0200

    feat: added ConfigServer service, connected all 3 services with it to have centralized configuration and added Env variables for DB credentails for each microservices

---
+++ configserver/src/test/java/com/eccomerce/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eccomerce.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
