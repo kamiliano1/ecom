56c8c31b367d9364a0a4247018d545eb85ba34cd
kamiliano1 <kamiliano1991@gmail.com>
2026-09-17 21:05:30 +0200

    feat: installed RabbitMQ and SpringBus to refresh the properties without restart and configured all services to work with it

--- order/src/main/java/com/ecommerce/order/Controller/OrderController.java
+++ order/src/main/java/com/ecommerce/order/Controller/OrderController.java
@@ -6,10 +6,7 @@ import com.ecommerce.order.dto.OrderResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
