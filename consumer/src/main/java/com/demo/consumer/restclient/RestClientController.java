b0bc68259d6dece7d83905bafccca0182f50ae98
kamiliano1 <kamiliano1991@gmail.com>
2026-09-21 20:50:02 +0200

    feat: added Feign and RestClient communication between services

---
+++ consumer/src/main/java/com/demo/consumer/restclient/RestClientController.java
@@ -0,0 +1,20 @@
+package com.demo.consumer.restclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/api/rest-client")
+@RequiredArgsConstructor
+public class RestClientController {
+
+    private final ProviderRestClient providerRestClient;
+
+    @GetMapping("/instance")
+    public String getInstance() {
+        return providerRestClient.getInstanceInfo();
+    }
+}
